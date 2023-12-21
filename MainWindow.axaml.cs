c1b5e3c8ac41b78cfdc345dd47328fb5438692a2
Chris Weinert <10633787+MrChrisWeinert@users.noreply.github.com>
2023-12-20 20:10:23 -0600

    Added a little extra help in case directories/files aren't in the right spot.

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -31,7 +31,7 @@ namespace MAMEIronXP
         private DateTime _startTimeCPress;
         private DateTime _startTimeVPress;
         private const int LONGPRESSMILLISECONDS = 3000;
-        private int JUMPDISTANCE=25;
+        private int JUMPDISTANCE = 25;
 
         public MainWindow()
         {
@@ -53,16 +53,20 @@ namespace MAMEIronXP
             string errorText;
             if (!File.Exists(_mameExe))
             {
-                errorText = $"{_mameExe} does not exist.";
+                errorText = $"Error: {_mameExe} was not found.";
                 Console.WriteLine(errorText);
+                Console.WriteLine("1) Ensure all prerequisite are met (https://github.com/MrChrisWeinert/MAMEIronXP#prerequisites)");
+                Console.WriteLine("2) Check the MAMEDirectory setting in the App.config to make sure you're pointed at your MAME executable.");
                 _logger.LogException(errorText, new Exception($"MAME executable ({_mameExe}) not found"));
                 Environment.Exit(1);
             }
             else if (!File.Exists(_catver))
             {
-                errorText = $"{_catver} does not exist.";
+                errorText = $"Error: {_catver} was not found.";
+                Console.WriteLine("1) Ensure all prerequisite are met (https://github.com/MrChrisWeinert/MAMEIronXP#prerequisites)");
+                Console.WriteLine("2) Verify that your MAMEDirectory setting in the App.config is correct and that catver.ini exists in that directory.");
                 Console.WriteLine(errorText);
-                _logger.LogException(errorText, new Exception($"{_catver} not found"));
+                _logger.LogException(errorText, new Exception($"catver.ini {_catver} not found"));
                 Environment.Exit(1);
             }
             else if (!File.Exists(_gamesJson))
@@ -72,7 +76,9 @@ namespace MAMEIronXP
             }
             else if (!Directory.Exists(_snapDirectory))
             {
-                errorText = $"{_snapDirectory} does not exist.";
+                errorText = $"Error: {_snapDirectory} was not found.";
+                Console.WriteLine("1) Ensure all prerequisite are met (https://github.com/MrChrisWeinert/MAMEIronXP#prerequisites)");
+                Console.WriteLine("2) Verify that your MAMEDirectory setting in the App.config is correct and that your roms directory exists that directory.");
                 Console.WriteLine(errorText);
                 _logger.LogException(errorText, new Exception($"Snap directory ({_snapDirectory}) not found"));
                 Environment.Exit(1);
@@ -80,7 +86,7 @@ namespace MAMEIronXP
             LoadGamesFromJSON();
             if (_games.Count == 0)
             {
-                errorText = $"_games count is zero.";
+                errorText = $"Error: Unable to load games.";
                 Console.WriteLine(errorText);
                 _logger.LogException(errorText, new Exception("Games did not load"));
                 Environment.Exit(1);
