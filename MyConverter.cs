bc620d1b373e5ca00bb6f92190ceb0557e536b5b
Chris Weinert <10633787+MrChrisWeinert@users.noreply.github.com>
2023-12-24 15:52:08 -0600

    Show the Pacman icon if a game is marked as a Favorite

---
+++ MyConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Avalonia.Data.Converters;
+//using Avalonia.Data;
+using System.Globalization;
+
+namespace MAMEIronXP
+{
+    public class MyConverter : IValueConverter
+    {
+        public static readonly MyConverter Instance = new();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                if (boolValue == true)
+                {
+                    return boolValue ? "1" : "";
+                }
+            }
+
+            throw new InvalidOperationException("Value must be a boolean");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
