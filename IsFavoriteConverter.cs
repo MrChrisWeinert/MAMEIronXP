709bcbc483d4396d70cbfd8df4fb67262478593a
Chris Weinert <10633787+MrChrisWeinert@users.noreply.github.com>
2023-12-26 17:47:25 -0600

    Rename the converter and also fix the Favorite icon issue

--- IsFavoriteConverter.cs
+++ IsFavoriteConverter.cs
@@ -4,9 +4,9 @@ using System.Globalization;
 
 namespace MAMEIronXP
 {
-    public class MyConverter : IValueConverter
+    public class IsFavoriteConverter : IValueConverter
     {
-        public static readonly MyConverter Instance = new();
+        public static readonly IsFavoriteConverter Instance = new();
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
@@ -14,8 +14,12 @@ namespace MAMEIronXP
             {
                 if (boolValue == true)
                 {
-                    return boolValue ? "1" : "";
+                    //Display the PacMan icon if the game is marked as a Favorite.
+                    //When using the Pac-Font font, a "1" corresponds to the PacMan icon.
+                    return "1";
                 }
+                //Otherewise, don't display the icon.
+                else return " ";
             }
 
             throw new InvalidOperationException("Value must be a boolean");
